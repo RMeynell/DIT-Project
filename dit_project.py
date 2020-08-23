e13111abb89342abb6e48e8db58d899b5d7edf3c
RMeynell <46811955+RMeynell@users.noreply.github.com>
2020-08-23 17:27:03 +1200

    Add files via upload
    Added Menu system, has basic questions and the start of hard questions

--- dit_project.py
+++ dit_project.py
@@ -5,61 +5,79 @@
 #import random
 import random
 
+#creating a menu system to navigate the program
+def menu():
+    user_choice = 11
+    while user_choice != 1 and user_choice != 2:
+        user_choice = int(input("""Please choose an option:
+(1). Basic Questions
+(2). Hard Questions
+(3). Choose your own questions\n"""))
+    if user_choice == 1:
+        basic_questions()
+    elif user_choice == 2:
+        hard_questions()
+    else:
+        print("end")
+        
+
+            
+
 #defining the function that will ask questions
-def questions():
+def basic_questions():
     score = 0
-    addition = "+"
-    subtraction = "-"
-    multiplication = "x"
     #asking the user what operation they would like to use
     operation = input("""What operation would you like to use:
 Addition,
 Subtraction,
 Multiplication?\n""").lower().strip()
-    #running the right operation
+    #setting operation to the correct symbol
     if operation == "addition":
-        #starting a loop that will ask the user questions
-        for i in range(0, 5):
-            number1 = random.randint(0,12)
-            number2 = random.randint(0,12)
-            answer = int(input("What is {} + {}?\n".format(number1, number2)))
-            #checking if the users answer is correct
-            if answer == number1 + number2:
-                print("Correct")
-                score += 1
-            else:
-                print("That is incorrect")
-        print("Your final score is: {}/5".format(score))
+        operation = "+"
     elif operation == "subtraction":
-        #starting a loop that will ask the user questions
-        for i in range(0, 5):
-            number1 = random.randint(0,12)
-            number2 = random.randint(0,12)
-            answer = int(input("What is {} - {}?\n".format(number1, number2)))
-            #checking if the users answer is correct
-            if answer == number1 - number2:
-                print("Correct")
-                score += 1
-            else:
-                print("That is incorrect")
-        print("Your final score is: {}/5".format(score))
+        operation = "-"
     elif operation == "multiplication":
-        #starting a loop that will ask the user questions
-        for i in range(0, 5):
-            number1 = random.randint(0,12)
-            number2 = random.randint(0,12)
-            answer = int(input("What is {} x {}?\n".format(number1, number2)))
-            #checking if the users answer is correct
-            if answer == number1 * number2:
+        operation = "x"
+    #asking the user how many questions they would like
+    user_question = int(input("How many questions would you like?\n"))
+    #creating a loop that will ask the user their given amount of questions
+    for i in range(0, user_question):
+        number1 = random.randint(0,12)
+        number2 = random.randint(0,12)
+        user_answer = int(input("What is {} {} {}?\n".format(number1, operation, number2)))
+        #checking if the users answer is correct
+        if operation == "+":
+            if user_answer == number1 + number2:
                 print("Correct")
                 score += 1
             else:
-                print("That is incorrect")
-        print("Your final score is: {}/5".format(score))
+                print("Incorrect")
+        elif operation == "-":
+            if user_answer == number1 - number2:
+                print("Correct")
+                score += 1
+            else:
+                print("Incorrect")
+        elif operation == "x":
+            if user_answer == number1 * number2:
+                print("Correct")
+                score += 1
+            else:
+                print("Incorrect")
+    print("Your final score is {}/{}".format(score, user_question))
+    menu = input("Would you like to return to the menu?\n").strip().lower()
+    if menu == "yes":
+        main()
 
-        
-questions()
+#defining function to give the user harder questions
+def hard_questions():
+    print("end")
 
+#defining function main to start the program
+def main():
+    menu()
+
+main()
 
 
 
