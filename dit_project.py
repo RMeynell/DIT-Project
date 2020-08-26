5414a1416df1d236a5742d34607e2938fbdd804e
RMeynell <46811955+RMeynell@users.noreply.github.com>
2020-08-27 09:44:07 +1200

    Add files via upload
    Changed a few things, added a history function but need to make it work

--- dit_project.py
+++ dit_project.py
@@ -8,12 +8,14 @@ import random
 # creating a menu system to navigate the program
 def menu():
     user_choice = 11
-    while user_choice <= 0 or user_choice >= 5:
+    while user_choice <= 0 or user_choice >= 4:
         try:
-            user_choice = int(input("""Please choose an option:
+            user_choice = int(input("""------------------------
+Please choose an option:
 (1). Basic Questions
 (2). Hard Questions
-(3). Exit the game\n"""))
+(3). History
+(4). Exit the game\n------------------------\n"""))
         except ValueError:
             print("Please enter a number")
     if user_choice == 1:
@@ -21,6 +23,8 @@ def menu():
     elif user_choice == 2:
         hard_questions()
     elif user_choice == 3:
+        history()
+    elif user_choice == 4:
         print("Quitting")
 
 # defining the function that will ask questions
@@ -31,10 +35,11 @@ def basic_questions():
     operation = 0
     while operation not in operations:
         try:
-            operation = input("""What operation would you like to use:
+            operation = input("""------------------------
+What operation would you like to use:
 (1). Addition,
 (2). Subtraction,
-(3). Multiplication?\n:""").lower().strip()
+(3). Multiplication?\n------------------------\n""").lower().strip()
         except ValueError:
             print("Please enter a number between 1 and 4")
 
@@ -66,8 +71,6 @@ def basic_questions():
             number2 = random.randint(0, 12)
         # asking the user the questions
         question_num = (i + 1)
-        print(number1)
-        print(number2)
         user_answer = 0
         while user_answer == 0:
             try:
@@ -90,7 +93,6 @@ def basic_questions():
             print("Incorrect")
         user_answer = 0
     print("Your final score is {}/{}".format(score, user_question))
-    print(prev_answers)
     menu = input("Would you like to return to the menu?\n").strip().lower()
     if menu == "yes":
         print("Returning to the menu")
@@ -105,12 +107,13 @@ def hard_questions():
     # asking the user what operation they would like to use
     OPERATIONS = ["1", "2", "3"]
     operation = 0
-    while operation not in operations:
+    while operation not in OPERATIONS:
         try:
-            operation = input("""What operation would you like to use:
+            operation = input("""------------------------
+What operation would you like to use:
 (1). Addition,
 (2). Subtraction,
-(3). Multiplication?\n:""").lower().strip()
+(3). Multiplication?\n------------------------\n""").lower().strip()
         except ValueError:
             print("Please enter a number between 1 and 4")
 
@@ -122,12 +125,29 @@ def hard_questions():
     elif operation == "3":
         operation = OP_SYMBOLS[2]
     # asking the user how many questions they would like
-    user_question = int(input("How many questions would you like?\n"))
+    user_question = -1
+    while user_question <= 0 or user_question > 50:
+        try:
+            user_question = int(input("How many questions would you like?\n"))
+        except ValueError:
+            print("Please enter a number above 0 and below 50")
     # creating a loop that will ask the user their given amount of questions
     for i in range(0, user_question):
         number1 = random.randint(0, 24)
         number2 = random.randint(0, 24)
-        user_answer = int(input("What is {} {} {}?\n".format(number1, operation, number2)))
+        # preventing negative numbers
+        while number1 < number2:
+            number1 = random.randint(0, 24)
+            number2 = random.randint(0, 24)
+        # asking the user the questions
+        question_num = (i + 1)
+        user_answer = 0
+        while user_answer == 0:
+            try:
+                user_answer = int(input("Question {}: {} {} {}\n= "
+                .format(question_num, number1, operation, number2)))
+            except ValueError:
+                print("Please input a number value")
         # checking if the users answer is correct
         if operation == "+":
             answer = number1 + number2
@@ -144,6 +164,10 @@ def hard_questions():
     menu = input("Would you like to return to the menu?\n").strip().lower()
     if menu == "yes":
         main()
+
+#defining history function
+def history():
+    print("History")
 
 # defining function main to start the program
 def main():
