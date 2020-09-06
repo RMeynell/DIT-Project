4cf983be69fb84ac05c40bd2c113eec6a7da4a29
RMeynell <46811955+RMeynell@users.noreply.github.com>
2020-09-07 10:46:04 +1200

    Add files via upload

--- dit_project.py
+++ dit_project.py
@@ -5,13 +5,15 @@
 # import random
 import random
 
+# asking the user for their name
+name = input("What is your name?\n")
 # defining dict to store users previous questions
 prev_answers = {
-    1 == ["Hello World"]
+    
     }
     
 # creating a menu system to navigate the program
-def menu():
+def menu(name):
     user_choice = 11
     while user_choice <= 0 or user_choice >= 4:
         try:
@@ -19,7 +21,8 @@ def menu():
 Please choose an option:
 (1). Questions
 (2). History
-(3). Exit the game\n------------------------\n"""))
+(3). View Your Info
+(4). Exit the game\n------------------------\n"""))
         except ValueError:
             print("Please enter a number")
     if user_choice == 1:
@@ -27,10 +30,17 @@ Please choose an option:
     elif user_choice == 2:
         history(prev_answers)
     elif user_choice == 3:
+        user_info(name)
+    elif user_choice == 4:
         print("Quitting...")
+    return name
 
 # defining the function that will ask questions
 def questions(prev_answers):
+    #setting the number of coins the user has
+    coins = 10
+    #setting continuous answer streak
+    streak = 0
     #asking the user what difficulty they would like
     difficulty = -1
     while difficulty != 1 and difficulty != 2:
@@ -100,8 +110,14 @@ What operation would you like to use:
         if user_answer == answer:
             print("Correct")
             score += 1
+            coins += 5
+            streak += 1
         else:
             print("Incorrect")
+            streak = 0
+        #giving the user a reward for having a streak
+        if streak == 5:
+            streak
         #saving the users answer to a dictionary
         prev_answers[Question] = [number1, operation, number2, user_answer]
         user_answer = 0
@@ -125,10 +141,80 @@ Returning to the menu""")
     total_questions = len(prev_answers)
     print("There are a total of {} possible questions to view".format(total_questions))
     view_questions = int(input("How many of these questions would you like to view?\n"))
-    
+   
+# defining function for the gambling part of the program
+def gamble():
+    print("ghe")
+
+# defining function to store the users info
+def user_info(name):
+    print("Name: {}".format(name))
+    print("Coins: {}".format(coins))
+    menu = input("Would you like to return to the menu?\n").strip().lower()
+    if menu == "yes":
+        print("Returning to the menu")
+        main()
+    else:
+        print("Quitting...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # defining function main to start the program
 def main():
-    menu()
+    menu(name)
 
 main()
+
+
+
+
+
+"""
+def questions():
+    coins = 0
+    streak = 0
+    # setting difficulty
+    difficulty = -1
+    while difficulty != 1 and difficulty != 2:
+        try:
+            difficulty = int(input("""------------------------
+What difficulty would you like?
+(1). Easy
+(2). Hard\n------------------------\n"""))
+        except:
+            print("Please enter 1 or 2")
+    # randomnizing the operation
+    OPERATIONS = [+, -, /, *]
+    random_op = random.randint(0,3)
+    final_op = OPERATIONS[random_op]
+    # asking the user the questions
+    for i in range(0, 5):
+    # setting the number range
+    if difficulty = 1:
+        number1 = random.randint(0, 12)
+        number2 = random.randint(0, 12)
+    else:
+        number1 = random.randint(0, 24)
+        number2 = random.randint(0, 24)
+    #preventing negative numbers
+        while number1 < number2:
+            number1 = random.randint(0, 12)
+            number2 = random.randint(0, 12)
+"""
