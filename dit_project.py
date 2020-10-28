b4f5aaaceec464498e7df47fcfd0d2e073b8e896
RMeynell <46811955+RMeynell@users.noreply.github.com>
2020-10-28 22:01:31 +1300

    Add files via upload

--- dit_project.py
+++ dit_project.py
@@ -9,14 +9,15 @@ import random
 coins = 1000
 streak = 0
 high_streak = 0
-user_items = ["", "", ""]
+user_items = []
 name = input("What is your Username?: ")
-
+shop_items = ["Pencil", "Protractor", "Eraser", "Compass", "Blank Book", "Textbook", "Calculator"]
+shop_prices = [10, 10, 15, 20, 30, 50, 100]
 # defining function for the menu system
 
-def menu(coins, name, streak, high_streak, user_items):
+def menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices):
     user_choice = 11
-    while user_choice <= 0 or user_choice >= 7:
+    while user_choice <= 0 or user_choice >= 8:
         try:
             user_choice = int(input("""
 ------------------------
@@ -24,9 +25,10 @@ Please choose an option:
 (1). Questions
 (2). Gambling
 (3). Shop
-(4). User Info
-(5). Program Info
-(6). Quit Program
+(4). Inventory
+(5). User Info
+(6). Program Info
+(7). Quit Program
 ------------------------
 """))
         except:
@@ -38,12 +40,14 @@ Please choose an option:
     elif user_choice == 2:
         gamble(coins, name, streak, high_streak)
     elif user_choice == 3:
-        shop(coins, user_items)
+        shop(coins, user_items, shop_items, shop_prices)
     elif user_choice == 4:
-        user_info(coins, name, streak, high_streak, user_items)
+        inventory(name, user_items)
     elif user_choice == 5:
-        info()
+        user_info(coins, name, streak, high_streak, user_items)
     elif user_choice == 6:
+        info()
+    elif user_choice == 7:
         print("Quitting...")
     
 # defining function to let the user choose their difficulty
@@ -135,7 +139,7 @@ def easy_questions(coins, streak, high_streak):
     if streak > high_streak:
         high_streak = streak
     # returning the user to the menu if they would like
-    menu(coins, name, streak, high_streak, user_items)
+    menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices)
     return streak 
     
 # defining function to ask the user hard questions
@@ -202,7 +206,7 @@ def hard_questions(coins, streak, high_streak):
                 print("Incorrect")
                 streak = 0
         # checking if the users streak can give them a reward
-        streak_prize = streak * 2
+        streak_prize = streak * 3
         if streak % 5 == 0 and streak > 0:
             print("You got a streak prize")
             print("Your streak reached {}".format(streak))
@@ -216,14 +220,14 @@ def hard_questions(coins, streak, high_streak):
     # checking if the users highest streak has been beaten
     if streak > high_streak:
         high_streak = streak
-    menu(coins, name, streak, high_streak, user_items)
+    menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices)
     return streak
 
 # defining function for the gambling feature
 def gamble(coins):
     if coins <= 29:
         print("You don't have enough coins")
-        menu(coins, name, streak, high_streak, user_items)
+        menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices)
     gamble = input("Would you like to gamble $30 (Y/N)").strip().lower()
     print(gamble)
     if gamble == "y":
@@ -235,68 +239,214 @@ def gamble(coins):
         if number < 5000:
             print("You won 20 coins")
             coins += 20
-            menu(coins, name, streak, high_streak, user_items)
+            menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices)
         elif number > 5000 and number < 8000:
             print("You won 30 coins")
             coins += 30
-            menu(coins, name, streak, high_streak, user_items)
+            menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices)
         elif number > 8000 and number < 9500:
             print("You won 50 coins")
             coins += 50
-            menu(coins, name, streak, high_streak, user_items)
+            menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices)
         elif number > 9500 and number < 9999:
             print("You won 100 coins")
             coins += 100
-            menu(coins, name, streak, high_streak, user_items)
+            menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices)
         elif number == 10000:
             print("You won 10,000 coins")
             coins += 10000
-            menu(coins, name, streak, high_streak, user_items)
+            menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices)
 
 # defining function for the shop
-def shop(coins, user_items):
-    # defining variables
-    shop_items = ["(1). Pencil", "(2). Blank Book", "(3). Calculator", "(4). Return to menu"]
+def shop(coins, user_items, shop_items, shop_prices):
     # showing the user items they can buy
     print("Welcome to the shop")
     print("You currently have {} coins".format(coins))
+    # checking if the user has bought all the items in the shop
+    if len(shop_items) == 0:
+        print("You have already bought all the items in the shop")
+        print("Returning to the menu")
+        menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices)
     print("--------------------------")
     for i in range(0, len(shop_items)):
-        print(shop_items[i])
-    print("--------------------------")
+        print("({}). {}".format((i + 1), shop_items[i]))
+    print("(0). Return to the menu")
+    print("--------------------------")   
     # checking the user enough coins to buy their selected item
     price_met = False    
     while price_met == False:
         try:
             item_buy = int(input("Which item would you like to buy today?: \n"))
         except:
-            print("Please enter a number between 1 and 4")
-        if item_buy == 1 and coins >= 10:
+            print("Please enter a number between 1 and 7")
+        # giving the user a pencil
+        if item_buy == 1 and shop_items[0] == "Pencil" and coins >= 10:
             price_met = True
-            print("You have successfuly bought a Pencil")
+            print("You have successfully bought a Pencil")
+            user_items.append("Pencil")
             coins -= 10
-            shop_items.remove[0]
-        elif item_buy == 2 and coins >= 30:
+            shop_items.remove("Pencil")
+        # giving the user a protractor
+        elif item_buy == 1 and shop_items[0] == "Protractor" and coins >= 10:
             price_met = True
-            print("You have successfuly bought a Blank Book")
-            book_no += 1
-            user_items[0] = "{}x Blank Book".format(book_no)
+            print("You have successfully bought a Protractor")
+            user_items.append("Protractor")
+            coins -= 10
+            shop_items.remove("Protractor")
+        elif item_buy == 2 and shop_items[1] == "Protractor" and coins >= 10:
+            price_met = True
+            print("You have successfully bought a Protractor")
+            user_items.append("Protractor")
+            coins -= 10
+            shop_items.remove("Protractor")
+        # giving the user an eraser
+        elif item_buy == 1 and shop_items[0] == "Eraser" and coins >= 15:
+            price_met = True
+            print("You have successfully bought an Eraser")
+            user_items.append("Eraser")
+            coins -= 15
+            shop_items.remove("Eraser")
+        elif item_buy == 2 and shop_items[1] == "Eraser" and coins >= 15:
+            price_met = True
+            print("You have successfully bought an Eraser")
+            user_items.append("Eraser")
+            coins -= 15
+            shop_items.remove("Eraser")
+        elif item_buy == 3 and shop_items[2] == "Eraser" and coins >= 15:
+            price_met = True
+            print("You have successfully bought an Eraser")
+            user_items.append("Eraser")
+            coins -= 15
+            shop_items.remove("Eraser")
+        # giving the user a compass
+        elif item_buy == 1 and shop_items[0] == "Compass" and coins >= 20:
+            price_met = True
+            print("You have successfully bought a Compass")
+            user_items.append("Compass")
+            coins -= 20
+            shop_items.remove("Compass")
+        elif item_buy == 2 and shop_items[1] == "Compass" and coins >= 20:
+            price_met = True
+            print("You have successfully bought a Compass")
+            user_items.append("Compass")
+            coins -= 20
+            shop_items.remove("Compass")
+        elif item_buy == 3 and shop_items[2] == "Compass" and coins >= 20:
+            price_met = True
+            print("You have successfully bought a Compass")
+            user_items.append("Compass")
+            coins -= 20
+            shop_items.remove("Compass")
+        # giving the user a blank book
+        elif item_buy == 1 and shop_items[0] == "Blank Book" and coins >= 30:
+            price_met = True
+            print("You have successfully bought a Blank Book")
+            user_items.append("Blank Book")
             coins -= 30
-            shop_items.remove[1]
-        elif item_buy == 3 and coins >= 100:
+            shop_items.remove("Blank Book")
+        elif item_buy == 2 and shop_items[1] == "Blank Book" and coins >= 30:
             price_met = True
-            print("You have successfuly bought a Calculator")
-            calc_no += 1
-            user_items[0] = "{}x Calculator".format(calc_no)
+            print("You have successfully bought a Blank Book")
+            user_items.append("Blank Book")
+            coins -= 30
+            shop_items.remove("Blank Book")
+        elif item_buy == 3 and shop_items[2] == "Blank Book" and coins >= 30:
+            price_met = True
+            print("You have successfully bought a Blank Book")
+            user_items.append("Blank Book")
+            coins -= 30
+            shop_items.remove("Blank Book")
+        elif item_buy == 4 and shop_items[3] == "Blank Book" and coins >= 30:
+            price_met = True
+            print("You have successfully bought a Blank Book")
+            user_items.append("Blank Book")
+            coins -= 30
+            shop_items.remove("Blank Book")
+        elif item_buy == 5 and shop_items[4] == "Blank Book" and coins >= 30:
+            price_met = True
+            print("You have successfully bought a Blank Book")
+            user_items.append("Blank Book")
+            coins -= 30
+            shop_items.remove("Blank Book")
+        # giving the user a textbook
+        elif item_buy == 1 and shop_items[0] == "Textbook" and coins >= 50:
+            price_met = True
+            print("You have successfully bought a Textbook")
+            user_items.append("Textbook")
+            coins -= 50
+            shop_items.remove("Textbook")
+        elif item_buy == 2 and shop_items[1] == "Textbook" and coins >= 50:
+            price_met = True
+            print("You have successfully bought a Textbook")
+            user_items.append("Textbook")
+            coins -= 50
+            shop_items.remove("Textbook")
+        elif item_buy == 3 and shop_items[2] == "Textbook" and coins >= 50:
+            price_met = True
+            print("You have successfully bought a Textbook")
+            user_items.append("Textbook")
+            coins -= 50
+            shop_items.remove("Textbook")
+        elif item_buy == 4 and shop_items[3] == "Textbook" and coins >= 50:
+            price_met = True
+            print("You have successfully bought a Textbook")
+            user_items.append("Textbook")
+            coins -= 50
+            shop_items.remove("Textbook")
+        elif item_buy == 5 and shop_items[4] == "Textbook" and coins >= 50:
+            price_met = True
+            print("You have successfully bought a Textbook")
+            user_items.append("Textbook")
+            coins -= 50
+            shop_items.remove("Textbook")
+        # giving the user a calculator
+        elif item_buy == 1 and shop_items[0] == "Calculator" and coins >= 100:
+            price_met = True
+            print("You have successfully bought a Calculator")
+            user_items.append("Calculator")
             coins -= 100
-            shop_items.remove[2]
-        elif item_buy == 4:
-            menu(coins, name, streak, high_streak, user_items)
+            shop_items.remove("Calculator")
+        elif item_buy == 2 and shop_items[1] == "Calculator" and coins >= 100:
+            price_met = True
+            print("You have successfully bought a Calculator")
+            user_items.append("Calculator")
+            coins -= 100
+            shop_items.remove("Calculator")
+        elif item_buy == 3 and shop_items[2] == "Calculator" and coins >= 100:
+            price_met = True
+            print("You have successfully bought a Calculator")
+            user_items.append("Calculator")
+            coins -= 100
+            shop_items.remove("Calculator")
+        elif item_buy == 4 and shop_items[3] == "Calculator" and coins >= 100:
+            price_met = True
+            print("You have successfully bought a Calculator")
+            user_items.append("Calculator")
+            coins -= 100
+            shop_items.remove("Calculator")
+        elif item_buy == 5 and shop_items[4] == "Calculator" and coins >= 100:
+            price_met = True
+            print("You have successfully bought a Calculator")
+            user_items.append("Calculator")
+            coins -= 100
+            shop_items.remove("Calculator")
+        elif item_buy == 6 and shop_items[5] == "Calculator" and coins >= 100:
+            price_met = True
+            print("You have successfully bought a Calculator")
+            user_items.append("Calculator")
+            coins -= 100
+            shop_items.remove("Calculator")
+        
+        elif item_buy == 0:
+            break
         else:
             print("You do not have enough coins to buy that item")
+    
     # asking the user if they would like to return to the menu
     menu_return = -1
     while menu_return <= 0 or menu_return >= 3:
+        if item_buy == 0:
+            break
         try:
             menu_return = int(input("""
 -----------------------------------------------
@@ -308,55 +458,33 @@ Would you buy more items or return to the menu?
         except:
             print("Please enter 1 or 2")
     if menu_return == 1:
-        shop(coins, user_items)
+        shop(coins, user_items, shop_items, shop_prices)
     else:
-        menu(coins, name, streak, high_streak, user_items)
+        menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices)
 
+# defining function for the inventory
+def inventory(name, user_items):
+    if len(user_items) == 0:
+            print("You have no items in your inventory to view")
+            print("Head to the shop to buy items")
+            print("Returning to the menu")
+            menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices)
+    else:
+        print("{}'s Inventory:".format(name))
+        for i in range(0, len(user_items)):
+            print(user_items[i])
+    # returning users to the menu
+    menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices)
+    
 # defining function to view user info
 def user_info(coins, name, streak, high_streak, user_items):
     print("Username: {}".format(name))
     print("You currently have {} coins".format(coins))
     print("Your highest streak is: {}".format(high_streak))
     print("Your current streak is: {}".format(streak))
-    view_inventory = "x"
-    while view_inventory != "y" and view_inventory != "n":
-        try:
-            view_inventory = input("Would you like to view your inventory? (Y/N)").strip().lower()
-        except:
-            ("Please enter Y or N")
-    if view_inventory == "y":
-        for i in range(0, len(user_items)):
-            print(user_items[i])
-    # returning users to the menu
-    menu(coins, name, streak, high_streak, user_items)
-
-# defining function to give the user information about the program
-def info():
-    print("""
-------------------------------------------------------------------------
-                               Questions:
-     You will be given a set of questions at your chosen difficulty
-            The two difficulties available are easy and hard
-            Easy gives you addition and subtraction questions
-The easy questions will use randomly generated numbers between 0 and 12
-   Hard gives you addition, subtraction, and multiplication questions
-The hard questions will use randomly generated numbers between 10 and 24
-------------------------------------------------------------------------
-                                 Coins:
-     For every easy question you get correct you will earn 10 coins
-     For every hard question you get correct you will earn 20 coins
-      You also get a reward for getting consecutive correct answers
-              Every time your streak passes a multiple of 5
-                   You will earn 2 times the multiple
-------------------------------------------------------------------------
-                               User Info:
-                   This screen displays your user name,
-           your current amount of coins, your current streak,
-                         and your highest streak
-------------------------------------------------------------------------""")
-    menu(coins, name, streak, high_streak, user_items)      
-# defining function main to start the program
+    
+# defining main
 def main():
-    menu(coins, name, streak, high_streak, user_items)
+    menu(coins, name, streak, high_streak, user_items, shop_items, shop_prices)
 
 main()
